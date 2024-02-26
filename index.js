614fb57bf90ca36a5fad802e020f7286ea9a5195
ahsanalijs <ahsanali45444@gmail.com>
2024-02-26 23:58:59 +0500

    add

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+app.get('/login', (req, res) => {
+    res.send('Hello User!')
+})
+app.get('/admin', (req, res) => {
+    res.send('Hello Admin!')
+})
+app.get('/new', (req, res) => {
+    res.send('Hello New!')
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
