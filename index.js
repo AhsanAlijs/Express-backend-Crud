ad05f4d1cf544959cb6d4b3a1be8c76062a9a7c9
ahsanalijs <ahsanali45444@gmail.com>
2024-02-27 00:37:49 +0500

    Complete Crud backend express js

--- index.js
+++ index.js
@@ -5,18 +5,52 @@ require('dotenv').config()
 // const port = 3000
 // middle Where
 app.use(cors());
+app.use(express.json());
+
+const arr = [];
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-app.get('/login', (req, res) => {
-    res.send('Hello User!')
+
+//get all users
+app.get('/api/v1/users', (req, res) => {
+    res.send(arr)
 })
-app.get('/admin', (req, res) => {
-    res.send('Hello Admin!')
+
+//add user
+app.post('/api/v1/users', (req, res) => {
+    const { title } = req.body;
+    arr.push({
+        title: title,
+        id: Date.now(),
+    })
+    res.send('user added successfully');
 })
-app.get('/new', (req, res) => {
-    res.send('Hello New!')
+
+//delete user
+app.delete('/api/v1/users/:id', (req, res) => {
+    const { id } = req.params
+    const index = arr.findIndex((user) => user.id === Number(id));
+    if (index === -1) {
+        res.send('user not found');
+        return
+    }
+    arr.splice(index, 1);
+    res.send('user deleted');
+})
+
+//edit user
+app.put('/api/v1/users/:id', (req, res) => {
+    const { title } = req.body;
+    const { id } = req.params;
+    const index = arr.findIndex((user) => user.id === Number(id));
+    if (index === -1) {
+        res.send('user not found');
+        return
+    }
+    arr[index].title = title
+    res.send('user edited successfully')
 })
 
 app.listen(process.env.PORT, () => {
